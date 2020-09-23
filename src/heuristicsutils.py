a3721a61dbb8af4516765f6c48fcfa63f17cabb7
SpecificGenerality <justiny@princeton.edu>
2020-09-23 13:49:36 -0500

    add heuristicsutils tests

--- src/heuristicsutils.py
+++ src/heuristicsutils.py
@@ -1,5 +1,5 @@
 import random
-from typing import List
+from typing import Callable, List
 
 from actioncard import ActionCard
 from card import Card
@@ -7,7 +7,7 @@ from treasurecard import TreasureCard
 from victorycard import VictoryCard
 
 
-def heuristic_select_cards(choices: List[Card], k: int, scoringFunction) -> List[Card]:
+def heuristic_select_cards(choices: List[Card], k: int, scoringFunction: Callable[[Card], float]) -> List[Card]:
         random.shuffle(choices)
         choices.sort(key=scoringFunction, reverse=True)
         cards = []
@@ -15,42 +15,46 @@ def heuristic_select_cards(choices: List[Card], k: int, scoringFunction) -> List
             cards.append(choices[i])
         return cards
 
-def heuristic_best_card(choices: List[Card], scoringFunction):
+def heuristic_best_card(choices: List[Card], scoringFunction: Callable[[Card], float]) -> Card:
         v = choices
         random.shuffle(v)
         v.sort(key=scoringFunction, reverse=True)
         return v[0]
 
-def get_best_TD_card(choices: List[Card]):
+def get_best_TD_card(choices: List[Card]) -> Card:
     return max(choices, key=lambda x: x.get_plus_cards())
 
-def has_plus_action_cards(choices: List[Card]):
+def has_plus_action_cards(choices: List[Card]) -> bool:
     return any(card.get_plus_actions() > 0 for card in choices)
 
-def get_first_plus_action_card(choices: List[Card]):
-    return next(c.get_plus_actions() > 0 for c in choices)
+def get_first_plus_action_card(choices: List[Card]) -> Card:
+    try:
+        return next(filter(lambda x: x.get_plus_actions() > 0, choices))
+    except StopIteration:
+        return None
 
-def get_lowest_treasure_card(choices: List[Card]):
-    return min(choices, key=lambda x: x.get_treasure())
+def get_lowest_treasure_card(choices: List[Card]) -> Card:
+    try:
+        return min(filter(lambda x: isinstance(x, TreasureCard), choices), key=lambda x: x.get_treasure())
+    except ValueError:
+        return None
 
-def has_treasure_cards(choices: List[Card]):
+def has_treasure_cards(choices: List[Card]) -> Card:
     return any(isinstance(card, TreasureCard) for card in choices)
 
-def get_first_no_action_card(choices: List[Card]):
-    return next(c.get_plus_actions() == 0 and isinstance(c, ActionCard) for c in choices)
+def get_first_no_action_card(choices: List[Card]) -> Card:
+    try:
+        return next(filter(lambda x: x.get_plus_actions() == 0 and isinstance(x, ActionCard), choices))
+    except StopIteration:
+        return None
 
-def get_max_plus_cards_card(choices: List[Card]):
+def get_max_plus_cards_card(choices: List[Card]) -> Card:
     card = max(choices, key=lambda x: x.get_plus_cards())
     return card if card.get_plus_cards() >= 2 else None
 
-def get_highest_VP_card(choices: List[Card]):
+def get_highest_VP_card(choices: List[Card]) -> Card:
     return max(choices, key=lambda x: x.get_victory_points())
 
-def has_excess_actions(choices: List[Card]):
-    return sum(card.get_plus_actions() for card in choices) - sum(1 if isinstance(card, ActionCard) else 0 for card in choices)
-
-def getExcessActionCard(choices: List[Card]):
-    if has_excess_actions(choices):
-        return get_first_no_action_card(choices)
-    else:
-        return None
+def has_excess_actions(choices: List[Card]) -> bool:
+    action_card_iter = filter(lambda x: isinstance(x, ActionCard), choices)
+    return sum(x.get_plus_actions() - 1 for x in action_card_iter) > 0
---
+++ tst/test_heuristicsutils.py
@@ -0,0 +1,21 @@
+import unittest
+
+from actioncard import Festival, Merchant, Smithy
+from heuristicsutils import *
+from treasurecard import Gold, Silver
+from victorycard import Estate
+
+
+class TestHeuristicsUtils(unittest.TestCase):
+    def setUp(self) -> None:
+        self.choices = [Estate(), Silver(), Gold(), Merchant(), Festival(), Smithy()]
+
+    def test_utils(self) -> None:
+        self.assertEquals(get_best_TD_card(self.choices), self.choices[-1])
+        self.assertTrue(has_plus_action_cards(self.choices))
+        self.assertEquals(get_first_plus_action_card(self.choices), self.choices[3])
+        self.assertEquals(get_lowest_treasure_card(self.choices), self.choices[1])
+        self.assertTrue(has_treasure_cards(self.choices))
+        self.assertEquals(get_first_no_action_card(self.choices), self.choices[-1])
+        self.assertEquals(get_highest_VP_card(self.choices), self.choices[0])
+        self.assertFalse(has_excess_actions(self.choices))
