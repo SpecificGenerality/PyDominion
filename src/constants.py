6c4a0847442239f9abd266b027711667f8e7b017
SpecificGenerality <justiny@princeton.edu>
2020-09-28 09:25:49 -0500

    test merchant

--- src/constants.py
+++ src/constants.py
@@ -4,12 +4,6 @@ from cursecard import Curse
 from victorycard import Gardens
 
 '''List of the Base set kingdom cards'''
-# TODO: Implement Vassal, Merchant
-BASE_CARDS = [Cellar, Chapel, Moat, \
-        Harbinger, Merchant, Village, Workshop, \
-        Bureaucrat, Gardens, Militia, Moneylender, Poacher, Remodel, Smithy, ThroneRoom, \
-        CouncilRoom, Festival, Laboratory, Library, Market, Mine, Witch, \
-        Artisan]
 
 BASE_CARD_NAME = {
         'Artisan': Artisan,
@@ -39,3 +33,5 @@ BASE_CARD_NAME = {
         'Witch': Witch,
         'Workshop': Workshop,
 }
+
+BASE_CARDS = [v for v in BASE_CARD_NAME.values()]
