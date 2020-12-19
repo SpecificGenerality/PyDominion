402e556f305b6e0b6b6860b253b9aef8a1d0c8cd
SpecificGenerality <justiny@princeton.edu>
2020-12-19 17:09:40 -0600

    can terminal states predict intermediate states

--- scripts/predictor.py
+++ scripts/predictor.py
@@ -1,4 +1,6 @@
+import os
 import pickle
+import sys
 from argparse import ArgumentParser
 from typing import Iterable
 
@@ -7,7 +9,7 @@ from config import GameConfig
 from enums import FeatureType, StartingSplit
 from env import DefaultEnvironment
 from player import Player, load_players
-from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.linear_model import LinearRegression, LogisticRegression, Ridge
 from state import DecisionResponse, State
 from tqdm import tqdm
 
@@ -35,7 +37,7 @@ def train(n: int, predict_model, config: GameConfig, players: Iterable[Player], 
     return reg.score(X, y)
 
 
-def predict(reg, n: int, config: GameConfig, players: Iterable[Player], **kwargs):
+def predict(reg, n: int, config: GameConfig, players: Iterable[Player], turn_break=sys.maxsize):
     env = DefaultEnvironment(config, players)
 
     X = np.zeros((n, len(env.game.state.feature)))
@@ -57,11 +59,23 @@ def predict(reg, n: int, config: GameConfig, players: Iterable[Player], **kwargs
     return reg.score(X, y)
 
 
+def evaluate_turn_break(reg, n: int, config: GameConfig, players: Iterable[Player]) -> np.array:
+    scores = []
+    for i in range(2, 30, 2):
+        score = predict(reg, n, config, players, turn_break=i)
+        scores.append(score)
+    return np.array(scores)
+
+
 def load_predict_model(reg_cls, alpha: float = None):
     if reg_cls == LinearRegression:
         return reg_cls()
     elif reg_cls == Ridge:
         return reg_cls(alpha=alpha)
+    elif reg_cls == LogisticRegression:
+        if alpha:
+            return reg_cls(max_iter=10e5, C=alpha)
+        return reg_cls(max_iter=10e5)
 
 
 if __name__ == '__main__':
@@ -69,12 +83,13 @@ if __name__ == '__main__':
     parser.add_argument('-n', required=True, default=10000, type=int, help='Number of games; number of rows in X')
     parser.add_argument('-path', required=True, type=str, help='Path to save (load) linreg model for train (predict)')
     parser.add_argument('-ftype', required=True, type=lambda x: {'full': FeatureType.FullFeature, 'reduced': FeatureType.ReducedFeature}.get(x.lower()))
-    parser.add_argument('-reg_cls', type=lambda x: {'ols': LinearRegression, 'ridge': Ridge}.get(x.lower()))
+    parser.add_argument('-reg_cls', type=lambda x: {'ols': LinearRegression, 'ridge': Ridge, 'logistic': LogisticRegression}.get(x.lower()))
     parser.add_argument('--sandbox', action='store_true', help='Uses no action cards when set.')
     parser.add_argument('--predict', action='store_true', help='Predicts using linear regressor and random rollouts')
     parser.add_argument('--players', nargs='+', type=str, choices=['R', 'BM', 'TDBM', 'UCT', 'MLP'], help='AI strategy')
     parser.add_argument('--models', nargs='+', type=str, help='Paths to AI strategy models')
-    parser.add_argument('--alpha', default=None, help='Ridge regression parameter')
+    parser.add_argument('--alpha', default=None, type=float, help='Ridge regression parameter')
+
     args = parser.parse_args()
 
     reg = load_predict_model(args.reg_cls, alpha=args.alpha)
@@ -90,6 +105,7 @@ if __name__ == '__main__':
         pickle.dump(reg, open(args.path, 'wb'))
     else:
         reg = pickle.load(open(args.path, 'rb'))
-        score = predict(reg, args.n, config, players)
+        # score = predict(reg, args.n, config, players)
+        scores = evaluate_turn_break(reg, args.n, config, players)
 
-        print(f'R_sq = {score}')
+        print(f'R_sq = {scores}')
