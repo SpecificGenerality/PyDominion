bc6634b4610e586d6e56b968c9cb5efe7dcd7cb4
SpecificGenerality <justiny@princeton.edu>
2020-12-21 14:51:02 -0600

    add mlp predictor

--- scripts/predictor.py
+++ scripts/predictor.py
@@ -1,24 +1,30 @@
 import pickle
-import sys
 from argparse import ArgumentParser
-from typing import Iterable
+from typing import Iterable, Tuple
 
 import numpy as np
+import torch
+import torch.nn as nn
 from config import GameConfig
 from enums import FeatureType, StartingSplit
 from env import DefaultEnvironment
+from mlp import MLP
 from player import Player, load_players
 from sklearn.linear_model import LinearRegression, LogisticRegression, Ridge
 from state import DecisionResponse, State
+from torch.utils.data.dataloader import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
 
-def train(n: int, predict_model, config: GameConfig, players: Iterable[Player], **kwargs) -> float:
+def sample_training_batch(n: int, p: float, config: GameConfig, players: Iterable[Player], one_hot=False) -> Tuple[np.array, np.array]:
     env = DefaultEnvironment(config, players)
+    X = []
+    y = []
 
-    X = np.zeros((n, len(env.game.state.feature)))
-    y = np.zeros(n)
+    rng = np.random.default_rng()
 
+    print('Generating training data from self-play...')
     for epoch in tqdm(range(n)):
         state: State = env.reset()
         done = False
@@ -29,47 +35,79 @@ def train(n: int, predict_model, config: GameConfig, players: Iterable[Player], 
             player.makeDecision(state, action)
             obs, reward, done, _ = env.step(action)
 
-        X[epoch] = obs.feature.to_numpy()
-        y[epoch] = reward
+            if rng.uniform(0, 1) < p:
+                X.append(obs.feature.to_numpy())
 
+        X.append(obs.feature.to_numpy())
+
+        # TODO: Fix this hardcode
+        if one_hot:
+            label = np.zeros(3)
+            label[(reward + 1)] = 1
+            y.append(label)
+        else:
+            y.extend([reward + 1] * (len(X) - len(y)))
+
+    return np.array(X), np.array(y)
+
+
+def train_mlp(X, y, model: nn.Module, epochs: int, **kwargs) -> float:
+    save_epochs = kwargs['save_epochs']
+    path = kwargs['path']
+    dataset = []
+
+    print('Generating dataset for dataloader...')
+    for i in tqdm(range(len(X))):
+        dataset.append((torch.tensor(X[i]).cuda(), torch.tensor(y[i]).cuda()))
+
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+
+    criterion = nn.CrossEntropyLoss()
+    optim = torch.optim.Adam(model.parameters())
+
+    model.cuda()
+    model.train()
+    writer = SummaryWriter()
+
+    print('Training MLP...')
+
+    for epoch in tqdm(range(epochs)):
+        running_loss = 0.0
+        for i, data in enumerate(dataloader):
+            inputs, labels = data
+
+            optim.zero_grad()
+            y_pred = model.forward(inputs)
+            loss = criterion(y_pred, labels)
+            loss.backward()
+            optim.step()
+
+            running_loss += loss.item()
+
+        if save_epochs > 0 and epoch % save_epochs == 0:
+            torch.save(model, path)
+
+        writer.add_scalar("Loss/train", running_loss, epoch)
+
+    writer.flush()
+    return test_mlp(X, y, model)
+
+
+def test_mlp(X: np.array, y: np.array, model: nn.Module) -> float:
+    model.eval()
+    inputs = torch.tensor(X).cuda()
+    labels = torch.tensor(y).cuda()
+
+    with torch.no_grad():
+        y_pred = model(inputs)
+
+    return (torch.argmax(y_pred, dim=1) == labels).sum().item() / len(y)
+
+
+def train_linear_model(X, y, predict_model) -> float:
     reg = predict_model.fit(X, y)
-    return reg.score(X, y)
-
-
-def predict(reg, n: int, config: GameConfig, players: Iterable[Player], turn_break=sys.maxsize):
-    env = DefaultEnvironment(config, players)
-
-    X = np.zeros((n, len(env.game.state.feature)))
-    y = np.zeros(n)
-
-    for epoch in tqdm(range(n)):
-        state: State = env.reset()
-        done = False
-        feature_done = False
-        while not done:
-            action = DecisionResponse([])
-            d = state.decision
-            player = players[d.controlling_player]
-            player.makeDecision(state, action)
-            obs, reward, done, _ = env.step(action)
-
-            if state.player_states[d.controlling_player].turns > turn_break:
-                X[epoch] = obs.feature.to_numpy()
-                feature_done = True
-
-        if not feature_done:
-            X[epoch] = obs.feature.to_numpy()
-        y[epoch] = reward
 
     return reg.score(X, y)
-
-
-def evaluate_turn_break(reg, n: int, config: GameConfig, players: Iterable[Player]) -> np.array:
-    scores = []
-    for i in range(2, 30, 2):
-        score = predict(reg, n, config, players, turn_break=i)
-        scores.append(score)
-    return np.array(scores)
 
 
 def load_predict_model(reg_cls, alpha: float = None):
@@ -84,37 +122,52 @@ def load_predict_model(reg_cls, alpha: float = None):
 
 
 def main(args: ArgumentParser):
-    reg = load_predict_model(args.reg_cls, alpha=args.alpha)
-
     config = GameConfig(split=StartingSplit.StartingRandomSplit, prosperity=False, num_players=len(args.players), sandbox=args.sandbox, feature_type=args.ftype)
 
     players = load_players(args.players, args.models, config)
 
     if not args.predict:
-        score = train(args.n, reg, config, players)
-
-        print(f'R_sq = {score}')
-
-        pickle.dump(reg, open(args.path, 'wb'))
+        if args.reg_cls == MLP:
+            X, y = sample_training_batch(args.n, args.p, config, players)
+            model = MLP(config.feature_size, (config.feature_size + 1) // 2)
+            acc = train_mlp(X, y, model, args.epochs, save_epochs=args.save_epochs, path=args.path)
+            print(f'Acc = {acc}')
+            torch.save(model, args.path)
+        else:
+            X, y = sample_training_batch(args.n, args.p, config, players)
+            reg = load_predict_model(args.reg_cls, alpha=args.alpha)
+            score = train_linear_model(X, y, reg)
+            print(f'R_sq = {score}')
+            pickle.dump(reg, open(args.path, 'wb'))
     else:
-        reg = pickle.load(open(args.path, 'rb'))
-        score = predict(reg, args.n, config, players)
-        print(f'R_sq = {score}')
-        # scores = evaluate_turn_break(reg, args.n, config, players)
+        if args.reg_cls == MLP:
+            model = torch.load(args.path)
+        else:
+            reg = pickle.load(open(args.path, 'rb'))
 
-        # print(f'R_sq = {scores}')
+        X, y = sample_training_batch(args.n, args.p, config, players)
+
+        if args.reg_cls == MLP:
+            acc = test_mlp(X, y, model)
+            print(f'Acc = {acc}')
+        else:
+            score = reg.score(X, y)
+            print(f'R_sq = {score}')
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-n', required=True, default=10000, type=int, help='Number of games; number of rows in X')
-    parser.add_argument('-path', required=True, type=str, help='Path to save (load) linreg model for train (predict)')
+    parser.add_argument('-path', required=True, type=str, help='Path to save (load) model for train (predict)')
+    parser.add_argument('-p', required=True, default=-1, type=float, help='Sampling probability for each turn. Pass negative value to only sample terminal state.')
     parser.add_argument('-ftype', required=True, type=lambda x: {'full': FeatureType.FullFeature, 'reduced': FeatureType.ReducedFeature}.get(x.lower()))
-    parser.add_argument('-reg_cls', type=lambda x: {'ols': LinearRegression, 'ridge': Ridge, 'logistic': LogisticRegression}.get(x.lower()))
+    parser.add_argument('-reg-cls', type=lambda x: {'ols': LinearRegression, 'ridge': Ridge, 'logistic': LogisticRegression, 'mlp': MLP}.get(x.lower()))
     parser.add_argument('--sandbox', action='store_true', help='Uses no action cards when set.')
     parser.add_argument('--predict', action='store_true', help='Predicts using linear regressor and random rollouts')
     parser.add_argument('--players', nargs='+', type=str, choices=['R', 'BM', 'TDBM', 'UCT', 'MLP'], help='AI strategy')
     parser.add_argument('--models', nargs='+', type=str, help='Paths to AI strategy models')
+    parser.add_argument('--epochs', type=int, help='Number of training epochs.')
+    parser.add_argument('--save-epochs', type=int, default=0, help='Number of epochs between saves')
     parser.add_argument('--alpha', default=None, type=float, help='Ridge regression parameter')
 
     args = parser.parse_args()
